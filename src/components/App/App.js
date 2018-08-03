1b4cfa72e7916946e0f07435b20bddee1201907e
joe1220 <joe1220@daum.net>
2018-08-03 23:18:59 +0900

    react structure

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        최상단
+      </div>
+    );
+  }
+}
+
+export default App;
